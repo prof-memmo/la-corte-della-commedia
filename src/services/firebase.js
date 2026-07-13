df80210a4a0a8d2e8f03fcb7331b286be0399abd
Guglielmo Piersanti <guglielmopiersanti@MacBook-Air-di-Guglielmo.local>
2026-07-13 22:57:02 +0200

    Aggiornata configurazione Firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 // Configurazione provvisoria per ambiente locale
 // L'utente sostituirà questi con le vere variabili d'ambiente
 const firebaseConfig = {
-  apiKey: "mock-api-key",
-  authDomain: "la-corte-commedia.firebaseapp.com",
-  projectId: "la-corte-commedia",
-  storageBucket: "la-corte-commedia.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyCgz52XehTx0qQQ1MkKtTnIM5LmjJKcPls",
+  authDomain: "la-corte-della-commedia.firebaseapp.com",
+  projectId: "la-corte-della-commedia",
+  storageBucket: "la-corte-della-commedia.firebasestorage.app",
+  messagingSenderId: "298739188542",
+  appId: "1:298739188542:web:f1613b00d197ff4859e26f"
 };
 
 const app = initializeApp(firebaseConfig);
