edfde36b893b89b7aa7ab9cf6d40e9968e051f97
Guglielmo Piersanti <guglielmopiersanti@MacBook-Air-di-Guglielmo.local>
2026-07-13 23:13:22 +0200

    Configurazione cartella docs per deploy dal main branch

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/la-corte-della-commedia/'
+  base: '/la-corte-della-commedia/',
+  build: {
+    outDir: 'docs'
+  }
 })
